8245a79a39ac283cc04d7310a4a202ce8ab51307
Francisco Altimari <francisco@altimari.nl>
2024-05-28 13:07:58 +0200

    Wrap in function to localize variables
    This is done so the current shell will not be poluted with the script
    variables, hopefully this will prevent any namespace clashes.

--- script.sh
+++ script.sh
@@ -2,74 +2,78 @@
 # MIT Francisco Altimari @1713980634
 
 # TODO: add error if not in a obsidian vault folder
-# TODO: change variables to use _local_ keyword?
 
-# Path of the global system obsidian dictionary
-global_path=~/.config/obsidian/Custom\ Dictionary.txt
+sync_dictionary () {
 
-# Path of your local vault dictionary
-# local_path=./.obsidian/dictionary.txt
-local_path=./dictionary.txt
+    # Path of the global system obsidian dictionary
+    local global_path=~/.config/obsidian/Custom\ Dictionary.txt
 
-if [[ $(uname) == 'Darwin' ]]; then
-    global_path=~/Library/Application\ Support/obsidian/Custom\ Dictionary.txt
-fi
+    # Path of your local vault dictionary
+    # local_path=./.obsidian/dictionary.txt
+    local local_path=./dictionary.txt
 
-create_local () {
-    echo "$1" > $local_path
-    echo date = $(date +%s) >> $local_path
-}
-
-check_file () {
-    # Check if file is editable, since we will append to it
-    if [[ -w "$1" ]]; then
-        edit_file "$1"
-    else
-        echo 'File cannot be edited'
-        read -ep 'Custom file path: ' custom_path
-        check_file "$custom_path"
-    fi
-}
-
-edit_file () {
-    # TODO: delete these debug lines
-	# cat "$1"
-	# printf '\n'
-	bat "$1"
-
-    old_words=$(tail "$1")
-
-    # Only trim last line if it is the checksum value
-    if [[ $(tail -1 "$1") == 'checksum_v'* ]]; then
-        old_words=$(sed '$d' "$1")
+    if [[ $(uname) == 'Darwin' ]]; then
+        local global_path=~/Library/Application\ Support/obsidian/Custom\ Dictionary.txt
     fi
 
-    echo $old_words
+    create_local () {
+        echo "$1" > $local_path
+        echo date = $(date +%s) >> $local_path
+    }
 
-    if ! [[ -w $local_path ]]; then
-        printf "\n"
-        echo 'Local dictionary not found (tried: "'$local_path'")'
-        read -p 'Create new one? [y/n]: ' yn
+    check_file () {
+        # Check if file is editable, since we will append to it
+        if [[ -w "$1" ]]; then
+            edit_file "$1"
+        else
+            echo 'File cannot be edited'
+            read -ep 'Custom file path: ' custom_path
+            check_file "$custom_path"
+        fi
+    }
 
-        case $yn in
-            [Yy]*) create_local "$old_words";;
-            *) return 0;;
-        esac
-    fi
+    edit_file () {
+        # TODO: delete these debug lines
+    	# cat "$1"
+    	# printf '\n'
+    	bat "$1"
 
-    # TODO: figure out how to make script stop?
-    echo 'more stuff?'
+        old_words=$(tail "$1")
 
-    # possible states:
-    # global (ahead)  |  global (behind)
-    # local (behind)  |  local   (ahead)
+        # Only trim last line if it is the checksum value
+        if [[ $(tail -1 "$1") == 'checksum_v'* ]]; then
+            old_words=$(sed '$d' "$1")
+        fi
+
+        echo $old_words
+
+        if ! [[ -w $local_path ]]; then
+            printf "\n"
+            echo 'Local dictionary not found (tried: "'$local_path'")'
+            read -p 'Create new one? [y/n]: ' yn
+
+            case $yn in
+                [Yy]*) create_local "$old_words";;
+                *) return 0;;
+            esac
+        fi
+
+        # TODO: figure out how to make script stop?
+        echo 'more stuff?'
+
+        # possible states:
+        # global (ahead)  |  global (behind)
+        # local (behind)  |  local   (ahead)
 
 
-	# last_line=$(tail )
+    	# last_line=$(tail )
+    }
+
+    echo 'Checking for dictionary file on: '
+    echo $global_path
+    printf '\n'
+
+    check_file "$global_path"
 }
 
-echo 'Checking for dictionary file on: '
-echo $global_path
-printf '\n'
-
-check_file "$global_path"
+sync_dictionary
